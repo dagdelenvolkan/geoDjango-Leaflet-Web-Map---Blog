455e8b928acd09f629fc8e5b62dce461fb53665b
dagdelenvolkan <dagdelen16@itu.edu.tr>
2021-03-16 17:17:32 +0300

    Model created

--- geoProject/geoApp/models.py
+++ geoProject/geoApp/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+
+class Saglik(models.Model):
+    ILCE_ADI = models.CharField(max_length=200)
+    ADI= models.CharField(max_length=200)
+    ALT_KATEGORI= models.CharField(max_length=200)
+    ADRES= models.CharField(max_length=400)
+    TELEFON= models.CharField(max_length=200)
+    WEBSITESI = models.CharField(max_length=200)
+    ACIL_SERVIS = models.CharField(max_length=200)
+    YATAK  =  models.IntegerField(null=True)
+    AMBULANS = models.CharField(max_length=200)
+    MAHALLE = models.CharField(max_length=200)
+    POINT = models.GeometryField()
+    
+    
+    def __str__(self):
+        return self.ADI
